466de17178b53b3e5adef27a7444c060bc94a875
Hyejung Na <hyejung.na01@gmail.com>
2024-08-21 03:34:30 +1000

    first commit

---
+++ src/component/Navbar.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  const menulist = [
+    '여성',
+    'Divided',
+    '남성',
+    '신생아/유아',
+    '아동',
+    'H&M HOME',
+    'Sale',
+    '지속가능성',
+  ];
+  return (
+    <div>
+      <div>
+        <div className='login-button'>
+          <FontAwesomeIcon icon={faUser} className='login-icon' />
+          <div>로그인</div>
+        </div>
+      </div>
+      <div className='nav-section'>
+        <img width={100} src='https://www2.hm.com/hm-logo.png'></img>
+      </div>
+      <div className='menu-area'>
+        <ul className='menu-list'>
+          {menulist.map((menu) => (
+            <li key={menu}>{menu}</li>
+          ))}
+        </ul>
+        <div className='input-container'>
+          <FontAwesomeIcon icon={faSearch} className='search-icon' />
+          <input
+            type='text'
+            className='search-input'
+            placeholder='Search...'
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/component/ProductCard.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const ProductCard = () => {
+  return (
+    <div>
+      <img
+        src='https://lp2.hm.com/hmgoepprod?set=source[/c0/3c/c03cd3305c0e599af18f6a1268b120db05f99796.jpg],origin[dam],category[],type[DESCRIPTIVESTILLLIFE],res[z],hmver[2]&call=url[file:/product/main]'
+        alt='tench-coat-image'
+      ></img>
+      <div>Conscious choice</div>
+      <div>벨티드 트월 코트</div>
+      <div>₩99000</div>
+      <div>신제품</div>
+    </div>
+  );
+};
+
+export default ProductCard;
