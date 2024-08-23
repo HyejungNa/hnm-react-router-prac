acfa1ed7e3a037a41051d8246a8bc1715e8b3b76
Hyejung Na <hyejung.na01@gmail.com>
2024-08-23 12:24:46 +1000

    Change api url address from local to json-server

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import ProductCard from '../component/ProductCard';
 import { Col, Container, Row } from 'react-bootstrap';
+import { useSearchParams } from 'react-router-dom';
 
 const ProductAll = () => {
   const [productList, setProductList] = useState([]);
+  const [query, setQuery] = useSearchParams();
 
   const getProducts = async () => {
-    let url = 'http://localhost:4000/products';
+    const searchQuery = query.get('q') || '';
+
+    console.log('쿼리값', searchQuery);
+    let url = `https://my-json-server.typicode.com/HyejungNa/hnm-react-router-prac/products?q=${searchQuery}`;
     let response = await fetch(url);
     let data = await response.json();
+    console.log(data);
     setProductList(data);
   };
 
   useEffect(() => {
     getProducts();
-  }, []);
+  }, [query]);
+
   return (
     <div>
       <Container>
